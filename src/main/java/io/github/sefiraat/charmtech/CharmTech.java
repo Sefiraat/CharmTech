48b97ccb88e389c776f6249374817c19a724c05c
BuildTools <unconfigured@null.spigotmc.org>
2021-04-30 13:25:11 +0100

    Initial Commit

---
+++ src/main/java/io/github/sefiraat/charmtech/CharmTech.java
@@ -0,0 +1,12 @@
+package io.github.sefiraat.charmtech;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CharmTech extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
---
+++ src/main/java/io/github/sefiraat/charmtech/lib/utils/Utils.java
@@ -0,0 +1,4 @@
+package io.github.sefiraat.charmtech.lib.utils;
+
+public class Utils {
+}
